dabf492922a0d35c3e4991127d264ce81351c17d
harshpandey002 <coding.harshp@gmail.com>
2023-05-24 16:49:11 +0530

    Configs

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,18 @@
-module.exports = {
+const { withExpo } = require('@expo/next-adapter');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withExpo({
   reactStrictMode: true,
-  transpilePackages: ["ui"],
-};
+  swcMinify: true,
+  transpilePackages: [
+    'react-native',
+    'expo',
+    'solito',
+    // Add more React Native / Expo packages here...
+  ],
+  experimental: {
+    forceSwcTransforms: true,
+  },
+});
+
+module.exports = nextConfig;
